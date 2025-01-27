baa6101efeda5fa78e9d0ef36a0b6884a796b61a
MostafaMaged00 <185123397+MostafaMaged00@users.noreply.github.com>
2025-01-27 13:38:00 -0500

    install ejs, add view folder , add 404 page everythin g working

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 //Imports
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 //Middleware
+// functions
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +16,10 @@ app.use(express.urlencoded({ extended: true }));
 //Connect to the database
 const conn = require("./server/models/db.js");
 conn();
+
+//Set Template Engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 //Routes
 //Get the user routes
@@ -25,6 +31,11 @@ app.use("/", quizRoute);
 //Get the book routes
 const bookRoute = require("./server/routes/bookRoutes.js");
 app.use("/", bookRoute);
+
+//404 error page
+app.use(function (req, res, next) {
+  res.render("404");
+});
 
 app.get("/", (req, res) => {
   res.send("<h1>This is start point</h1>");
