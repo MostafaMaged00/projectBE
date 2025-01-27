8ee46ac851dfd8a121ae91dabad7bc9b83432a7d
MostafaMaged00 <185123397+MostafaMaged00@users.noreply.github.com>
2025-01-27 09:26:29 -0500

    First commit to build BackEnd boilerplate

---
+++ server.js
@@ -0,0 +1,21 @@
+//Imports
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Routes
+app.get("/", (req, res) => {
+  res.send("This is start point");
+});
+//Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
